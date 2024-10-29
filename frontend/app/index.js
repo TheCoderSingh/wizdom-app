8f571cae582939fd53632f4cb331209a607116f4
Jaskaran Singh <thecodersingh@gmail.com>
2024-10-29 02:18:24 -0700

    Install Expo, nativewind, expo-router

---
+++ frontend/app/index.js
@@ -0,0 +1,12 @@
+import { View, Text, StatusBar } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello, world!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default App;
