fcd6568d62abab318e5b0307bd4b878809d1fb2f
Jaskaran Singh <thecodersingh@gmail.com>
2024-10-31 01:35:40 -0700

    Create splash screen

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,6 +1,23 @@
 import { View, Text, StatusBar } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+// Prevent the splash screen from auto-hiding
+SplashScreen.preventAutoHideAsync();
 
 const App = () => {
+  useEffect(() => {
+    const delaySplashScreen = async () => {
+      // Artificial delay
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+
+      // Hide the splash screen after the delay
+      await SplashScreen.hideAsync();
+    };
+
+    delaySplashScreen();
+  }, []);
+
   return (
     <View>
       <Text>Hello, world!</Text>
