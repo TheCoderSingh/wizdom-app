7f008da021fb103a01e996f691433bd38b8177a5
shadmanhossain <shadmanh@sfu.ca>
2024-11-27 21:29:52 -0800

    Added some new dependencies and code for userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,29 +1,99 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  name: String,
-  gender: String,
-  location: String,
-  hometown: String,
-  website: String,
-  role: String,
-  currentTitle: String,
-  previousTitles: [String],
-  yearsOfExperience: Number,
-  currentCompany: String,
-  previousCompanies: [String],
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: false,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  hometown: {
+    type: String,
+    required: false,
+  },
+  website: {
+    type: String,
+    required: false,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  currentTitle: {
+    type: String,
+    required: false,
+  },
+  previousTitles: [
+    {
+      type: String,
+      required: false,
+    },
+  ],
+  yearsOfExperience: {
+    type: Number,
+    required: true,
+  },
+  currentCompany: {
+    type: String,
+    required: false,
+  },
+  previousCompanies: [
+    {
+      type: String,
+      required: false,
+    },
+  ],
   education: {
     school: String,
     degree: String,
+    required: false,
   },
-  skills: [String],
-  techStack: [String],
-  bio: String,
-  connectWith: String,
-  learningGoals: String,
-  expertiseSkills: String,
+  skills: [
+    {
+      type: String,
+      required: true,
+      minSkills: 1,
+    },
+  ],
+  techStack: [
+    {
+      type: String,
+      required: false,
+    },
+  ],
+  bio: {
+    type: String,
+    required: false,
+  },
+  connectWith: {
+    type: String,
+    required: true,
+  },
+  learningGoals: {
+    type: String,
+    required: true,
+  },
+  expertiseSkills: {
+    type: String,
+    required: true,
+  },
   matches: [
     {
       type: mongoose.Schema.ObjectId,
