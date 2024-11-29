f77b740b613b6c98dcffe6ee81885ef613306eea
shadmanhossain <shadmanh@sfu.ca>
2024-11-28 18:34:41 -0800

    "Updated backend and frontend code: modified userModel.js and server.js, and added fb-watchman dependency to package.json and package-lock.json"

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // backend/server.js
 const express = require("express");
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const connectDB = require("./config/MongoDBConnection");
 
